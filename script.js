ee086e0ba120052903e35390e726b1d0f79a039a
séverine zapata <severine.zapata@orange.fr>
2023-07-17 21:34:45 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+var button = document.getElementById("Telecharger-jeu");
+button.addEventListener("click", function() {
+  window.location.href = "https://www.ea.com/fr/games/the-sims/the-sims-4/download";
+});
